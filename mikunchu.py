cdec0b7daf90474c0a4fc1bd34be2d9f8d87c334
cametan001 <Masashi.Kameda+github@gmail.com>
2011-05-15 06:00:28 +0900

    気になる事務のあのコのおっぱいに触れる

--- mikunchu.py
+++ mikunchu.py
@@ -3,6 +3,19 @@
 
 import sys, commands, os, os.path, pygtk, gtk
 
+## インストールコマンド群
+installCommands = (
+    ("gksudo add-apt-repository ppa:khf03353/ppa-kaorin",
+     "gksudo apt-get update",
+     'gksudo "apt-get -y install mikutube"'), # 一つ目のチェックボックスのコマンド群
+    ("gksudo add-apt-repository ppa:khf03353/ppa-kaorin",
+     "gksudo apt-get update",
+     'gksudo "apt-get -y install mikukabe"'), # 二つ目のチェックボックスのコマンド群
+    ("gksudo add-apt-repository ppa:khf03353/ppa-kaorin",
+     "gksudo apt-get update",
+     'gksudo "apt-get -y install mikukabe"') # 三つ目以降は、このように増やしていく
+    )
+
 class MikunchApp:
     def __init__(self):
         # Set the Glade file
@@ -15,6 +28,8 @@ class MikunchApp:
             "on_btnOK_clicked" : self.on_btnOK_clicked,
             "on_TopLevel_destroy" : self.on_TopLevel_destroy \
             }
+        self.chkMikutube = self.wTree.get_object("chkMikutube")
+        self.chkMikukabe = self.wTree.get_object("chkMikukabe")
         self.wTree.connect_signals(dic)
 
         self.mainWindow = self.wTree.get_object("TopLevel")
@@ -25,12 +40,25 @@ class MikunchApp:
         gtk.main_quit()
 
     def on_btnOK_clicked(self, widget):
+        # 適用を行ってアプリケーションを終了する
+        if self.chkMikutube.get_active:
+            # みくつべ♪インストールコマンドを実行
+            [self.execCommand(cmd) for cmd in installCommands[0]]
+        if self.chkMikutube.get_active:
+            # みくかべ♪インストールコマンドを実行
+            [self.execCommand(cmd) for cmd in installCommands[1]]
         # ウィンドウを閉じてアプリケーションを終了する
         gtk.main_quit()
 
     def on_btnCancel_clicked(self,widget):
         # ウィンドウを閉じてアプリケーションを終了する
         gtk.main_quit()
+
+    def execCommand(self, command):
+        print command                   # 受け渡されたコマンドのデバッグ用プリント
+        ret = commands.getoutput(command)
+        print ret                       # 実行結果のデバッグ用プリント
+        return ret
 
 if __name__ == "__main__":
     MikunchApp()
